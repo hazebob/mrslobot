06045bb2df7da74c5c74174433dc1a77ee85eeaa
yunki <bob@heem.net>
2015-10-30 17:05:00 +0900

    기분

--- scripts/command.js
+++ scripts/command.js
@@ -3,7 +3,7 @@
 module.exports = function(robot) {
 
 	// return robot.hear(/(js).*/i, function(msg) {
-	return robot.hear(/(오늘기분).*/i, function(msg) {
+	return robot.hear(/(기분).*/i, function(msg) {
 		var re = robot.http("http://slowalk.co.kr/wp-content/themes/slowalk/ajax.php?mode=slodaytoday").header('Content-Type', 'text/html').get()(function(err, res, body) {
 				msg.send('제 오늘 기분은요.');
 				msg.send(body);
