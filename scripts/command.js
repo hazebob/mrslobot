49b8e86cc1552fd202e86fd37a7d966621841953
yunki <bob@heem.net>
2015-10-30 17:03:35 +0900

    오늘 기분

--- scripts/command.js
+++ scripts/command.js
@@ -2,8 +2,12 @@
 //
 module.exports = function(robot) {
 
-  return robot.hear(/(js).*/i, function(msg) {
-    return msg.send("js is on");
-  });
+	// return robot.hear(/(js).*/i, function(msg) {
+	return robot.hear(/(오늘기분).*/i, function(msg) {
+		var re = robot.http("http://slowalk.co.kr/wp-content/themes/slowalk/ajax.php?mode=slodaytoday").header('Content-Type', 'text/html').get()(function(err, res, body) {
+				msg.send('제 오늘 기분은요.');
+				msg.send(body);
+		});
+	});
 
 };
