d357ac40cec4b9fa74aaa5acf7e5bb12dc26c8c3
yunkimoon <yunki@slowalk.co.kr>
2016-01-13 09:06:04 +0900

    mrslo.heem.net api

--- scripts/command.js
+++ scripts/command.js
@@ -1,13 +1,13 @@
-// command
-//
-module.exports = function(robot) {
+// // command
+// //
+// module.exports = function(robot) {
 
-	// return robot.hear(/(js).*/i, function(msg) {
-	return robot.respond(/(기분).*/i, function(msg) {
-		var re = robot.http("http://slowalk.co.kr/wp-content/themes/slowalk/ajax.php?mode=slodaytoday").header('Content-Type', 'text/html').get()(function(err, res, body) {
-				msg.send('제 오늘 기분은요.');
-				msg.send(body);
-		});
-	});
+// 	// return robot.hear(/(js).*/i, function(msg) {
+// 	return robot.respond(/(기분).*/i, function(msg) {
+// 		var re = robot.http("http://slowalk.co.kr/wp-content/themes/slowalk/ajax.php?mode=slodaytoday").header('Content-Type', 'text/html').get()(function(err, res, body) {
+// 				msg.send('제 오늘 기분은요.');
+// 				msg.send(body);
+// 		});
+// 	});
 
-};
+// };
---
+++ scripts/mrsloapi.js
@@ -0,0 +1,15 @@
+// mrsloapi
+//
+module.exports = function(robot) {
+
+	// return robot.hear(/(js).*/i, function(msg) {
+	return robot.respond(/(.*)/i, function(msg) {
+		var input = msg.match[1];
+		var input = encodeURIComponent(input);
+		var url = 'http://mrslo.heem.net/api/' + input;
+		var re = robot.http(url).header('Content-Type', 'text/html').get()(function(err, res, body) {
+				msg.send(body);
+		});
+	});
+
+};
