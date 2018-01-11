b25da46cdd6d34ead1d92ad45cd2d59557322ef6
Moon Yunki <yunki@slowalk.co.kr>
2018-01-11 11:06:36 +0900

    script

--- scripts/mrsloapi.js
+++ scripts/mrsloapi.js
@@ -6,7 +6,7 @@ module.exports = function(robot) {
 	return robot.respond(/(.*)/i, function(msg) {
 		var input = msg.match[1];
 		var input = encodeURIComponent(input);
-		var url = 'http://localhost:4000/api/' + input;
+		var url = 'http://mrslo.heem.net/api/' + input;
 		var re = robot.http(url).header('Content-Type', 'text/html').get()(function(err, res, body) {
 				msg.send(body);
 		});
