71a40b4ae00c7c16b41bf0bcea98f149f4f9d56d
Moon Yunki <yunki@slowalk.co.kr>
2018-01-11 11:27:35 +0900

    감정

--- scripts/mrsloapi.js
+++ scripts/mrsloapi.js
@@ -5,6 +5,9 @@ module.exports = function(robot) {
 	// return robot.hear(/(js).*/i, function(msg) {
 	return robot.respond(/(.*)/i, function(msg) {
 		var input = msg.match[1];
+		if(input.indexOf('감정') !== -1) {
+			return false;
+		}
 		var input = encodeURIComponent(input);
 		var url = 'http://mrslo.heem.net/api/' + input;
 		var re = robot.http(url).header('Content-Type', 'text/html').get()(function(err, res, body) {
